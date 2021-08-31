7b3c8a803d3a8ffb6a2ffea35e387fc8e1dd2632
Fall77 <arielalejandrojoelseco@gmail.com>
2021-08-31 19:02:18 -0300

    added parseInt and a div to show the result

--- clase-1/js/app.js
+++ clase-1/js/app.js
@@ -1,7 +1,10 @@
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
-
+let numero = parseInt(prompt("Ingrese un numero"));
 //alert(`${nombre} ${apellido}`);
+
+let div = document.querySelector('div');
+div.innerHTML = numero * 7; 
 
 let p = document.querySelector('p');
 p.innerHTML = `Hola ${nombre} ${apellido}`;
