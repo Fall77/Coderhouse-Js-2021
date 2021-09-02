2f6f61ac720b8109812499e324b0bcd7d986fd77
Fall77 <arielalejandrojoelseco@gmail.com>
2021-09-02 16:45:08 -0300

    agregue la clase 2 -Conditionals

---
+++ clase-2/js/app.js
@@ -0,0 +1,35 @@
+let fondo = prompt("Selecciona un color entre: azul; verde; rosa; violeta; amarillo.");
+
+let p = document.querySelector('p');
+let div = document.querySelector('div');
+
+function colorAleatorio(){       
+    let r = Math.random() * 255
+	let g = Math.random() * 255
+	let b = Math.random() * 255
+    document.body.style.backgroundColor = 'rgb('+r+','+g+', '+b+')';
+}
+    
+
+if(fondo == "azul"){
+    document.body.style.backgroundImage = "url('img/backBlue.JPG')";
+    document.body.style.backgroundColor = "#2a2a72";
+}else if(fondo == "verde"){
+    document.body.style.backgroundImage = "url('img/backGreen.JPG')";
+    document.body.style.backgroundColor = "#5aff15";    
+}else if(fondo == "rosa"){
+    document.body.style.backgroundImage = "url('img/backPink.JPG')";
+    document.body.style.backgroundColor = "#c81f70";    
+}else if(fondo == "violeta"){
+    document.body.style.backgroundImage = "url('img/backViolet.JPG')";
+    document.body.style.backgroundColor = "#647dee";    
+}else if(fondo == "amarillo"){
+    document.body.style.backgroundImage = "url('img/backYellow.JPG')";
+    document.body.style.backgroundColor = "#ffdd00";      
+}else{
+    div.innerHTML = `No se encontro el color ${fondo} <br> <br> Se generaran colores aleatorios mientras tanto <br> <br> F5 para recargar`
+    colorAleatorio();
+    setInterval(colorAleatorio, 1000); 
+}
+
+p.innerHTML = `Seleccionaste el color ${fondo}`;
